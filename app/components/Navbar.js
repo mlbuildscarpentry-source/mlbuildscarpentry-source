472a46985102c5fe85fd275ddcc496e71a172c1d
mlbuildscarpentry-source <mlbuildscarpentry@gmail.com>
2026-07-31 16:32:55 -0400

    Add navigation bar

---
+++ app/components/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <strong>MLBUILDS</strong>
+      </div>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="#services">Services</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
