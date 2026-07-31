b30f5672fc12bd6d253f7f75900d00c2353119f2
mlbuildscarpentry-source <mlbuildscarpentry@gmail.com>
2026-07-31 19:00:33 -0400

    Fix premium navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,22 +1,21 @@
 export default function Navbar() {
   return (
     <nav>
-      <a href="/">
+      <a href="/" aria-label="MLBUILDS homepage">
         <img
           src="/images/mlbuildslogo.png"
           alt="MLBUILDS Carpentry"
           className="logo"
+          style={{ height: "58px", width: "auto" }}
         />
       </a>
 
-      <div className="nav-links">
+      <div>
         <a href="/">Home</a>
         <a href="#services">Services</a>
         <a href="#projects">Projects</a>
         <a href="#about">About</a>
-        <a href="#contact" className="quote-button">
-          Free Quote
-        </a>
+        <a href="#contact">Free Quote</a>
       </div>
     </nav>
   );
