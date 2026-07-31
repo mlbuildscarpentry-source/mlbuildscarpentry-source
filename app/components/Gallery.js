35d03402cfea9000cc46c5f95a9a273ddb4f6592
mlbuildscarpentry-source <mlbuildscarpentry@gmail.com>
2026-07-31 19:19:34 -0400

    Simplify project gallery

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,35 +1,34 @@
+const projects = [
+  {
+    image: "/images/IMG_4251.JPG",
+    title: "Custom Carpentry",
+  },
+  {
+    image: "/images/IMG_6562.JPG",
+    title: "Deck Construction",
+  },
+  {
+    image: "/images/IMG_4370.JPG",
+    title: "Exterior Carpentry",
+  },
+  {
+    image: "/images/JPEG image-4FD0-85C1-7B-0.jpeg",
+    title: "Custom Build",
+  },
+];
+
 export default function Gallery() {
-  const projects = [
-    {
-      image: "/images/IMG_4251.JPG",
-      title: "Custom Carpentry Project",
-    },
-    {
-      image: "/images/IMG_6562.JPG",
-      title: "Deck Project",
-    },
-    {
-      image: "/images/IMG_4370.JPG",
-      title: "Exterior Project",
-    },
-    {
-      image: "/images/JPEG image-4FD0-85C1-7B-0.jpeg",
-      title: "Custom Build",
-    },
-  ];
-
   return (
-    <section id="projects">
-      <h2>Featured Projects</h2>
+    <div className="gallery-grid">
+      {projects.map((project) => (
+        <figure className="project-card" key={project.image}>
+          <img src={project.image} alt={project.title} />
 
-      <div className="gallery">
-        {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <img src={project.image} alt={project.title} />
+          <figcaption>
             <h3>{project.title}</h3>
-          </div>
-        ))}
-      </div>
-    </section>
+          </figcaption>
+        </figure>
+      ))}
+    </div>
   );
 }
