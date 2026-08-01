9d369b2ee0f1914bc102f075b6b110ed7c545ae6
mlbuildscarpentry-source <mlbuildscarpentry@gmail.com>
2026-08-01 07:50:38 -0400

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://mlbuildscarpentry.ca",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
